0664651c91aa0c78968ff9fa7d1f92a45db44aed
Anvita-Vegesna <65541350+Anvita-Vegesna@users.noreply.github.com>
2020-05-18 18:00:30 +0700

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,42 @@
+
+function question(){
+
+var word=document.getElementById('answer').value;
+
+if(word=='a'|| word==='A'|| word==='24')
+{
+alert ("Correct!");
+
+ }
+else{
+alert("Wrong!");
+}
+}
+
+
+function questio(){
+
+var Word=document.getElementById('ans').value;
+
+if(Word=='d'|| Word==='D'|| Word==='print("Hello World")')
+{
+alert ("Correct!");
+
+ }
+else{
+alert("Wrong!");
+}
+}
+function questi(){
+
+var Word=document.getElementById('answerss').value;
+
+if(Word=='c'|| Word==='C'|| Word==='===')
+{
+alert ("Correct!");
+
+ }
+else{
+alert("Wrong!");
+}
+}
